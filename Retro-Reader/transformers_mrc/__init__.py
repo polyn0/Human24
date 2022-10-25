81a01bc39f7a1cf47456b74fbf7ec135c17b6be0
Jeong Eunhye <62336723+JeongEunhye00@users.noreply.github.com>
2022-10-25 13:55:08 +0900

    Add files via upload

---
+++ Retro-Reader/transformers_mrc/__init__.py
@@ -0,0 +1,4 @@
+from .tokenizer_koelectra import KoElectraTokenizer
+from .configuration_koelectra import KoElectraConfig
+from .modeling_electra import ElectraForQuestionAnsweringAVPool
+from .modeling_outputs import QuestionAnsweringModelOutput
