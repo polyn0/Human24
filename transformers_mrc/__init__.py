cc1f32d9a56632dc185e8c82ceece8b94bb7687b
Jeong Eunhye <62336723+JeongEunhye00@users.noreply.github.com>
2022-10-25 13:53:20 +0900

    Delete transformers_mrc directory

--- transformers_mrc/__init__.py
+++
@@ -1,4 +0,0 @@
-from .tokenizer_koelectra import KoElectraTokenizer
-from .configuration_koelectra import KoElectraConfig
-from .modeling_electra import ElectraForQuestionAnsweringAVPool
-from .modeling_outputs import QuestionAnsweringModelOutput
